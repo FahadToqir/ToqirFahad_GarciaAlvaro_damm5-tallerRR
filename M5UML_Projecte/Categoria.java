a2b9ebddd6e92ac73196dde3606ce9ad464ca2bd
alvaroga45 <cf21alvaro.garcia@iesjoandaustria.org>
2022-02-22 02:33:41 +0100

    Classe Categori

---
+++ M5UML_Projecte/Categoria.java
@@ -0,0 +1,15 @@
+/*Classe categoria del programa */
+public class Categoria {
+	private String nom;
+	
+	public String ( String nom ) {
+		this.Categoria = nom;
+		}
+		
+	public String getNom() {
+		return this.nom;
+		}
+	public void setNom (String nom) {
+		this.nom = nom
+		}
+	
