b43fe06a98bf3c3418d248441e2f176620f5929d
FahadToqir <cf19fahad.toqir@iesjoandaustria.org>
2022-02-22 13:51:23 -0500

    producte

---
+++ Producte.java
@@ -0,0 +1,27 @@
+public class Producte {
+  private String nom;
+  private int cantidad;
+  
+  public Producte(String nombre,  int cantidad) {
+        this.nom = nombre ;
+        this.cantidad = cantidad;
+  }
+
+  public String getNom() {
+        return this.Nom;
+  }
+  
+  public void setNom(String Nom) {
+        this.Nom = nombre;
+    }
+
+  public int getCantidad() {
+        return this.Cantidad;
+  }
+
+  public void setCantidad(int Cantidad) {
+       this.Cantidad = cantidad;
+  }
+}
+        
+
