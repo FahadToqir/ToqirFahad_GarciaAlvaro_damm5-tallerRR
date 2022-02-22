7145521f0dd8fc7f56005d5676937db17e61282c
FahadToqir <cf19fahad.toqir@iesjoandaustria.org>
2022-02-22 14:59:01 -0500

    llista3

--- Llista.java
+++ Llista.java
@@ -9,25 +9,42 @@ public class Llista {
    }
    
    public static void crearProducto() {
-      System.out.println("Nom: ");
-      String nom = entrada.netxtLine().strip();
+      System.out.println("Nombre del Producto: ");
+      String nom = entrada.nextLine().strip();
       if (nom.isBlank) {
          System.out.println("Error: Introduzca minimo una letra o numero!");
          return;
       }
+
       for(Producte a : productos) {
-          if(p.getNom().equals(nom)) {
-            System.out.println("Error: El Producto que quiere crear ja existe!")
+          if(a.getNom().equals(nom)) {
+            System.out.println("Error: El Producto que quiere crear ja existe!");
             return;
           }
+      }
+      String producto = Entrada.readLine(nom);
+
+      System.out.println("Categoria?");
+      String categoria = entrada.nextLine().strip();
+      if(categoria.isBlank) {
+         System.out.println("Error: Introduzca minimo una letra o numero!");
+         return;
+      }
+      
+
       System.out.println("Quantitat?");
-      String Qt = entrada.netxtLine().strip();
-      if (Qt.isBlank){
+      String Qt = entrada.nextLine().strip();
+      if(!UtilString.esEnter(Qt)) {
+         System.out.println("Error: Introduzca una cantidad valida!");
+      }
+      if(Qt.isBlank){
          Qt = 1;
       }
-      if (Qt < 0) {
-         System.out.println("Error: Introduzca una cantidad valida!")
+      if(Qt < 0) {
+         System.out.println("Error: Introduzca una cantidad valida!");
          return;
+      }
+      int cantidad = Integer.parseInt(Qt);
   }
 
 
